abd04b58d27317a28dd349bb4768b7626fccde05
Kurt Miller <kmiller9393@gmail.com>
2024-04-15 18:22:26 -0600

    add initial components for expense tracking

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpenses />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Balance from "./components/Balance.vue";
+import Header from "./components/Header.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+
+import { computed, ref } from "vue";
+
+const transactions = ref([
+  {
+    id: 1,
+    text: "Flower",
+    amount: -19.99,
+  },
+  {
+    id: 2,
+    text: "Salary",
+    amount: 299.97,
+  },
+  {
+    id: 3,
+    text: "Book",
+    amount: -10,
+  },
+  {
+    id: 4,
+    text: "Camera",
+    amount: 150,
+  },
+]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, cv) => {
+    return acc + cv.amount;
+  }, 0);
+});
+
+// get income
+
+// get expenses
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <!-- <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>$500</span><button class="delete-btn">x</button>
+    </li> -->
+    <li
+      :key="transaction.id"
+      v-for="transaction in transactions"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
